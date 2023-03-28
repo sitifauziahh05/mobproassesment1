c21572597135c63f320120539c337997efe57c55
siti <stfauziahh05@gmail.com>
2023-03-28 22:00:33 +0700

    Assesment1

--- app/src/main/java/org/d3if6706210130/mykalendarevent/MainActivity.kt
+++ app/src/main/java/org/d3if6706210130/mykalendarevent/MainActivity.kt
@@ -1,11 +1,66 @@
 package org.d3if6706210130.mykalendarevent
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import org.d3if6706210130.mykalendarevent.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mButton = binding.button1
+
+        val startTime = "2023-03-1T09:00:00"
+        val endTime = "2023-03-1T12:00:10"
+
+        val mSimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val mStartTime = mSimpleDateFormat.parse(startTime)
+        val mEndTime = mSimpleDateFormat.parse(endTime)
+
+        mButton.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_EDIT)
+            mIntent.type = "vnd.android.cursor.item/event"
+            mIntent.putExtra("beginTime", mStartTime.time)
+            mIntent.putExtra("time", true)
+            mIntent.putExtra("rule", "FREQ=YEARLY")
+            mIntent.putExtra("endTime", mEndTime.time)
+            mIntent.putExtra("title", "Event")
+            startActivity(mIntent)
+        }
+
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener{ view, year, month, dayofmonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayofmonth)
+            updateLable(myCalendar)
+        }
+        binding.btnDatePicker.setOnClickListener{
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun updateLable(mycalendare: Calendar){
+        val myformat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myformat, Locale.UK)
+        binding.tvDate.setText(sdf.format(mycalendare.time))
     }
 }
